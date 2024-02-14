46336372fe44f885b29c7e278b77a3766a0abffc
gmendzy <mendoza.geoff@csu.fullerton.edu>
2024-02-14 11:06:31 -0800

    implemented playlist recommendation

--- app.py
+++ app.py
@@ -17,20 +17,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('search.html')
 
+@app.route('/generate_playlist', methods=['POST'])
+def generate_recommendations():
+    
+    song_name = request.form.get('song_name')
 
-@app.route('/search', methods=['POST'])
-def search():
-    query = request.form.get('query')
-    results = search_spotify(query)
-    return render_template('index.html', results=results)
+    try:
 
-def search_spotify(query):
-    results = sp.search(q=query, type='track', limit=5)
+        search_results = sp.search(q=song_name, type='track', limit=1)
+        track_id = search_results['tracks']['items'][0]['id']
 
-    return results['tracks']['items']['id']
+        recommendations = sp.recommendations(seed_tracks=[track_id], limit=5)
 
+        if 'tracks' in recommendations:
+            playlist_data = recommendations['tracks']
+            return render_template('results.html', playlist_data=playlist_data)
+        else:
+            return render_template('error.html', error_message ='No recommendations found.')
+    except Exception as e:
+        return render_template('error.html', error_message=str(e))
+    
 if __name__ == '__main__':
     app.run(debug=True)
 
