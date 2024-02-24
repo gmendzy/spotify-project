68626f9f3035a6803129c0431f83382760250eaf
gmendzy <mendoza.geoff@csu.fullerton.edu>
2024-02-23 16:43:57 -0800

    added css and html files, organized flask code

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 from spotipy import Spotify
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 from dotenv import load_dotenv
 import os
 
@@ -15,30 +15,57 @@ sp = Spotify(client_credentials_manager=client_credentials_manager)
 
 app = Flask(__name__)
 
+saved_song = {}
+
+
+
 @app.route('/')
 def index():
-    return render_template('search.html')
+    return render_template('index.html')
 
-@app.route('/generate_playlist', methods=['POST'])
+
+@app.route('/search', methods=['POST'])
+def search():
+    song_name = request.form.get('song_name')
+    results = sp.search(q=song_name, limit=5)
+    return render_template('search_results.html', results=results['tracks']['items'])
+
+
+
+@app.route('/choose_song', methods=['POST', 'GET'])
+def choose_song():
+    global saved_song
+    song_id = request.form.get('song_id')
+    saved_song = sp.track(song_id)
+    return redirect('/questionnaire')   
+
+
+
+@app.route('/questionnaire')
+def questionnaire():
+    return render_template('questionnaire.html')
+
+
+@app.route('/generate_playlist_from_questionnaire', methods=['POST'])
 def generate_recommendations():
     
-    song_name = request.form.get('song_name')
+    if 'saved_song' not in globals():
+        return "No song selected. Please go back and select a song."
 
-    try:
+    mood = request.form.get('mood')
+    location = request.form.get('location')
 
-        search_results = sp.search(q=song_name, type='track', limit=1)
-        track_id = search_results['tracks']['items'][0]['id']
-
-        recommendations = sp.recommendations(seed_tracks=[track_id], limit=5)
-
-        if 'tracks' in recommendations:
-            playlist_data = recommendations['tracks']
-            return render_template('results.html', playlist_data=playlist_data)
-        else:
-            return render_template('error.html', error_message ='No recommendations found.')
-    except Exception as e:
-        return render_template('error.html', error_message=str(e))
+    if mood == 'happy':
+        recommendations = sp.recommendations(seed_tracks=[saved_song['id']], target_energy=0.8, limit=5)
+    elif mood == 'sad':
+        recommendations = sp.recommendations(seed_tracks=[saved_song['id']], seed_genres=['sad'], target_energy=0.1, limit=5)
     
+    if location == 'home':
+        recommendations = sp.recommendatins(seed_tracks=[saved_song['id']], seed_genres=['chill', 'jazz'], target_energy=0.4, limit=5)
+
+    if 'tracks' in recommendations:
+        playlist_data = recommendations['tracks']
+        return render_template('results.html', playlist_data=playlist_data)
+
 if __name__ == '__main__':
     app.run(debug=True)
-
