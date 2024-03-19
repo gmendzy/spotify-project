a6571e1aad959d240a3df603abbd379951567c53
gmendzy <mendoza.geoff@csu.fullerton.edu>
2024-03-19 12:13:28 -0700

    Rewrote code in app.py to adjust song attributes dynamically

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 from dotenv import load_dotenv
 import os
+import datetime
 
 load_dotenv()
 client_id = os.getenv("CLIENT_ID")
@@ -35,10 +36,11 @@ def search():
 
 @app.route('/choose_song', methods=['POST', 'GET'])
 def choose_song():
-    global saved_song
+    global saved_song 
+    
     song_id = request.form.get('song_id')
     saved_song = sp.track(song_id)
-    return redirect('/questionnaire')   
+    return redirect('/questionnaire')  
 
 
 
@@ -49,14 +51,19 @@ def questionnaire():
 
 @app.route('/generate_playlist_from_questionnaire', methods=['POST'])
 def generate_recommendations():
-    
+
     if 'saved_song' not in globals():
         return "No song selected. Please go back and select a song."
     
-    mood = request.form.get('mood')
-    target_valence = 0
-    target_energy = 0
     
+    mood = request.form.get('mood')
+    activity = request.form.get('activity')
+
+    saved_song_attributes = sp.audio_features(saved_song['id'])[0]
+
+    target_features = saved_song_attributes.copy()    
+
+
     artist_id = saved_song['artists'][0]['id']
     artist = sp.artist(artist_id)
 
@@ -64,20 +71,40 @@ def generate_recommendations():
     seed_genre = genres[0] if genres else None
 
     if 'happy' in mood:
-        target_energy += 0.4
-        target_valence += 0.3
-    elif 'sad' in mood:
-        target_energy += 0.2
-        target_valence = 0
+        target_features['valence'] += 0.4 # Increase relative valence
+    if 'sad' in mood:
+        target_features['valence'] -= 0.2 # Decrease relative valence
+    if 'studying' in activity:
+        target_features['energy'] -= 0.2 # Decrease relative energy
+    if 'party' in activity:
+        target_features['energy'] += 0.2 # Increase relative energy
+    if 'chilling' in activity:
+        target_features['tempo' ] -= 10 # Decrease relative tempo
+    
+    
+    # Ensure that the target features are within the valid range
+    target_features['valence'] = min(max(target_features['valence'], 0), 1)
+    target_features['energy'] = min(max(target_features['energy'], 0), 1)
+    target_features['tempo'] = min(max(target_features['tempo'], 0), 200)
 
+    recommendations = sp.recommendations(
+        seed_tracks =  [saved_song['id']],
+        seed_genres = [seed_genre],
+        target_valence = target_features['valence'],
+        target_energy = target_features['energy'],
+        target_tempo = target_features['tempo'],
+        limit = 5
 
-    recommendations = sp.recommendations(seed_tracks=[saved_song['id']], seed_genres=[seed_genre], target_valence=target_valence, target_energy=target_energy, limit=5)
+    )
+
+        
+
+    
 
     if 'tracks' in recommendations:
         playlist_data = recommendations['tracks']
         return render_template('results.html', playlist_data=playlist_data)
     else:
         return "No recommendations were found with the desired mood."
-
 if __name__ == '__main__':
     app.run(debug=True)
